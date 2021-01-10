d5172b6fe3ffe0601ec7fd0c14c6eb4b43107360
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-01-10 12:22:29 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
+import { Routes, Route, Link as RouterLink } from "react-router-dom";
 import AuthorsPage from "./pages/AuthorsPage";
-import { Flex, Heading } from "@chakra-ui/react"
+import BooksPage from "./pages/BooksPage";
+import UserPage from "./pages/UserPage";
+import { Flex, Box, Divider, Heading, Link } from "@chakra-ui/react"
 
 export default function App() {
   return (
     <Flex direction="column" align="center" width="75%" mx="auto">
-      <Heading as="h1">Personal Library</Heading>
-      <AuthorsPage />
+      <Flex align="center" justify="space-between" w="100%" mx="5">
+        <Link to="/" as={RouterLink}>
+          <Heading as="h1">Personal Library</Heading>
+        </Link>
+        <Box d="flex">
+          <Link to="/" as={RouterLink}>
+            <h1>Authors</h1>
+          </Link>
+          <Divider orientation="vertical" />
+          <Link to="/users" as={RouterLink}>
+            <h1>Users</h1>
+          </Link>
+          <Divider orientation="vertical" />
+          <Link to="/books" as={RouterLink}>
+            <h1>Books</h1>
+          </Link>
+        </Box>
+      </Flex>
+      <Routes>
+        <Route path="/" element={<AuthorsPage />} />
+        <Route path="/users" element={<UserPage />} />
+        <Route path="/books" element={<BooksPage />} />
+      </Routes>
     </Flex>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import { CSSReset, ChakraProvider, theme } from "@chakra-ui/react"
 import App from "./App";
 
@@ -19,10 +20,12 @@ const client = new ApolloClient({
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <ChakraProvider theme={theme}>
-      <CSSReset />
-      <App />
-    </ChakraProvider>
+    <Router>
+      <ChakraProvider theme={theme}>
+        <CSSReset />
+        <App />
+      </ChakraProvider>
+    </Router>
   </ApolloProvider>,
   rootElement
 );
